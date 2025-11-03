dcc7ee34b03b62cb4e33346cd7cfbf937aa79c92
mytoandeptrai <67469007+mytoandeptrai@users.noreply.github.com>
2025-11-03 19:04:10 +0700

    fix: fix common pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,35 +3,37 @@
 import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
-import { ROUTES } from "@/utils/routes";
+import { VStack } from "@/components/utilities";
 
 export default function NotFound() {
   const router = useRouter();
 
   return (
-    <div className="absolute top-1/2 left-1/2 mb-16 -translate-x-1/2 -translate-y-1/2 items-center justify-center text-center">
-      <span className="from-foreground bg-linear-to-b to-transparent bg-clip-text text-[10rem] leading-none font-extrabold text-transparent">
-        404
-      </span>
-      <h2 className="font-heading my-2 text-2xl font-bold">
-        Something&apos;s missing
-      </h2>
-      <p>
-        Sorry, the page you are looking for doesn&apos;t exist or has been
-        moved.
-      </p>
-      <div className="mt-8 flex justify-center gap-2">
-        <Button onClick={() => router.back()} variant="default" size="lg">
-          Go back
-        </Button>
-        <Button
-          onClick={() => router.push(ROUTES.HOME)}
-          variant="ghost"
-          size="lg"
-        >
-          Back to Home
-        </Button>
-      </div>
-    </div>
+    <>
+      <VStack
+        justify="center"
+        align="center"
+        spacing={0}
+        className="relative w-full min-h-screen p-0 overflow-hidden text-foreground"
+      >
+        <div className="w-full max-w-xl text-center">
+          <span className="from-foreground bg-linear-to-b to-transparent bg-clip-text text-[10rem] leading-none font-extrabold text-transparent">
+            404
+          </span>
+          <h1 className="mb-3 font-bold text-muted text-title-md text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-5xl">
+            Something&apos;s missing
+          </h1>
+          <p className="text-base text-gray-500 mb-9">
+            Sorry, the page you are looking for doesn&apos;t exist or has been
+            moved.
+          </p>
+          <div className="mt-8 flex justify-center gap-2">
+            <Button onClick={() => router.back()} variant="default" size="lg">
+              Go back
+            </Button>
+          </div>
+        </div>
+      </VStack>
+    </>
   );
 }
